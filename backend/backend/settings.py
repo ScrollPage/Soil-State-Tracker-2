667ed3e307128053362ec1bb6be488b899dd0274
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2021-04-13 13:19:45 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -188,8 +188,8 @@ CORS_ORIGIN_WHITELIST = (
 # smtp
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', local.EMAIL_HOST_PASSWORD)
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', local.EMAIL_HOST_USER)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', local.EMAIL_HOST_USER)
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', local.EMAIL_HOST_PASSWORD)
 EMAIL_PORT = 587
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
