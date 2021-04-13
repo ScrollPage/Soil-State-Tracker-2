6e1de5ee20813ec54a9e4594f1a0b2d61bad751c
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2021-04-13 16:36:35 +0300

    Fix

--- backend/chat/service.py
+++ backend/chat/service.py
@@ -1,5 +1,6 @@
 from rest_framework.exceptions import AuthenticationFailed, PermissionDenied, ParseError
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 from channels.generic.websocket import WebsocketConsumer
 
