a6d187569aeb49faebf6ed02cdaa76d905aaff99
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2021-04-13 17:49:45 +0300

    Admin consumer

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,12 +1,14 @@
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 
 import json
 from asgiref.sync import async_to_sync
 
+from chat.models import Chat
 from .service import UpgradedWebsocketConsumer
 from .models import Chat, Message
 from client.models import Client
-from .permissions import UserInChat
+from .permissions import UserInChat, IsAdmin
 
 class ChatConsumer(UpgradedWebsocketConsumer):
     '''Консумер для чатов'''
@@ -14,6 +16,7 @@ class ChatConsumer(UpgradedWebsocketConsumer):
 
     # Connect/Disconnect
     def connect(self):
+        print(self.scope['user'])
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
         async_to_sync(self.channel_layer.group_add)(
@@ -50,13 +53,18 @@ class ChatConsumer(UpgradedWebsocketConsumer):
         self.send_message(content)
 
     # Main part
+    def prepare_data(data):
+        data['chat'] = get_object_or_404(Chat, id=self.room_name)
+        data['user'] = get_object_or_404(get_user_model(), id=data['user'])
+        return data
+
     commands = {
         'fetch_messages': fetch_messages,
         'new_message': new_message,
     }
 
     def receive(self, text_data):
-        data = self.prepare_data(text_data)
+        data = self.validate(text_data)
         self.check_permissions(data)
         self.commands[data.pop('command')](self, data)
 
@@ -90,3 +98,34 @@ class ChatConsumer(UpgradedWebsocketConsumer):
     def chat_message(self, event):
         message = event['message']
         self.send(text_data=json.dumps(message))
+
+
+# class AdminPanelConsumer(UpgradedWebsocketConsumer):
+#     '''Консумер админ-панели'''
+#     permissions = [IsAdmin()]
+#     permissions_by_command = {
+
+#     }
+
+#     def connect(self):
+#         self.room_group_name = 'admin_panel'
+#         async_to_sync(self.channel_layer.group_add)(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+
+#         self.accept()
+    
+#     def disconnect(self, close_code):
+#         async_to_sync(self.channel_layer.group_discard)(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+
+#     def prepare_data(data):
+#         pass
+
+#     commads = {}
+
+#     def receive(self, text_data):
+#         pass
